a3881732f53bc45f05e2eb6242fcfc9ec60fd916
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-17 15:12:50 -0300

    Update index.blade.php

--- resources/views/ItensEntrada/index.blade.php
+++ resources/views/ItensEntrada/index.blade.php
@@ -11,43 +11,45 @@
 
 <table class="table table-sm">
     @section('titulo','Itens Entradas')
-    @foreach ($ItensEntrada as $ItensEntrada)
-    <thead>
-        <th>precocompra</th>
-        <th>quantidade</th>
-        <th>unidade</th>
-        <th>ipi</th>
-        <th>frete</th>
-        <th>icms</th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </thead>
-    <thead>
-        <td>{{$ItensEntrada->precocompra}}</td>
-        <td>{{$ItensEntrada->quantidade}}</td>
-        <td>{{$ItensEntrada->unidade}}</td>
-        <td>{{$ItensEntrada->ipi}}</td>
-        <td>{{$ItensEntrada->frete}}</td>
-        <td>{{$ItensEntrada->icms}}</td>
-      
-            <form action="{{route('itensentradas.edit', ['id' => $ItensEntrada->id])}}" method="post">
-                @csrf
-                <input type="submit" class="btn btn-primary" value="alterar">
-            </form>
-        </td>
+   <div>
+        <table class="table table-sm">
+            @foreach ($ItensEntrada as $ItensEntrada)
+            <thead>
+                <th>precocompra</th>
+                <th>quantidade</th>
+                <th>unidade</th>
+                <th>ipi</th>
+                <th>frete</th>
+                <th>icms</th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </thead>
+            <thead>
+                <td>{{$ItensEntrada->precocompra}}</td>
+                <td>{{$ItensEntrada->quantidade}}</td>
+                <td>{{$ItensEntrada->unidade}}</td>
+                <td>{{$ItensEntrada->ipi}}</td>
+                <td>{{$ItensEntrada->frete}}</td>
+                <td>{{$ItensEntrada->icms}}</td>
 
-        <td>
-            <form action="{{route('itensentradas.delete', ['id' => $ItensEntrada->id])}}">
-            @csrf
-            @method('DELETE')
+                    <form action="{{route('itensentradas.edit', ['id' => $ItensEntrada->id])}}" method="post">
+                        @csrf
+                        <input type="submit" class="btn btn-primary" value="alterar">
+                    </form>
+                </td>
 
-            <input type="submit" class="btn btn-primary" value="apagar">
-            </form>
-        </td>
-    </thead>
-    
-@endforeach
+                <td>
+                    <form action="{{route('itensentradas.delete', ['id' => $ItensEntrada->id])}}">
+                    @csrf
+                    @method('DELETE')
+
+                    <input type="submit" class="btn btn-primary" value="apagar">
+                    </form>
+                </td>
+            </thead>
+
+        @endforeach
 
         <td>
             <form action="{{route('itensentradas.create')}}" method="post">
