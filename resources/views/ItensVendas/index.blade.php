713c64d74305fa750bc34bf9f74022c9a5d540ec
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-17 11:10:35 -0300

    Update index.blade.php

--- resources/views/ItensVendas/index.blade.php
+++ resources/views/ItensVendas/index.blade.php
@@ -8,15 +8,15 @@
 @section('conteudo')
 
 <table class="table table-sm">
-    @section('titulo','ItensVendas')
       <td>
-        
-            <form action="{{route('itensvendas.create')}}" method="post">
+                <form action="{{route('itensvendas.create')}}" method="post">
                     @csrf
                     <input type="submit" class="btn btn-primary" name="formulario" value="adicionar novo itens para a venda">
                 </form>
                 
-            </td>
+       </td>
+    @section('titulo','ItensVendas')
+    
     @foreach ($ItensVendas as $Itens_Venda )
         <thead>
            
