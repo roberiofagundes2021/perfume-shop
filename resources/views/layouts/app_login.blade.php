05a3a690ef6e0f1e5264e59da90b676d5742d825
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-17 13:33:24 -0300

    Update app_login.blade.php

--- resources/views/layouts/app_login.blade.php
+++ resources/views/layouts/app_login.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
