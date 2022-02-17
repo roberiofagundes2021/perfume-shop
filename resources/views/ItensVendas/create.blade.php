0a0d757d6b48f3e5590dcb6017e49b02cfa48614
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-17 19:33:44 -0300

    Update create.blade.php

--- resources/views/ItensVendas/create.blade.php
+++ resources/views/ItensVendas/create.blade.php
@@ -13,17 +13,7 @@
         <div class="row g-3">
 
             @section('titulo','Cadastro de Itens Vendas')
-            @if($errors->any())
-                        <div class="alert alert-danger">
-                       
-                            @foreach($errors->all() as $error)
-
-                                <label>{{$error}}</label><br>
-                            @endforeach
-                        
-                        </div>
-
-                    @endif
+           
             <div class="col">
                 <label for="quantidade_itens">quantidade de itens</label><br>
                 <input type="text" class="form-control" name="quantidade_itens" id="quantidade_itens">
@@ -31,8 +21,7 @@
                 <label for="valor">valor</label>
                 <input type="text" class="form-control" name="valor" id="valor"><br>
 
-                <input type="submit" class="btn btn-primary" value="cadastrar">
-            </div>
+           
                <label  for="produto_id">produto</label>
               <select name="produto_id" id="produto_id" class="form-control">
                      @foreach($Produto as $produto)
@@ -42,18 +31,12 @@
                      @endforeach
                 </select>
                 
-            <div class="col">
+            
                 
                 {{-- pega a variavel passada pelo metodo create do controller --}}
                 
-                <select class="form-control" name="produto_id">
-                    @foreach($Produto as $produto)
-                    <option value="{{$produto->produto_id}}">
-                        {{ $produto->nome }}
-                    </option>
-                     @endforeach
-                </select>
-
+            
+                    <input type="hind" name="" value="get->id">
 
                  <label for="venda_id">venda</label>
                 {{-- pega a variavel passada pelo metodo create do controller --}}
@@ -75,6 +58,9 @@
                             <label>{{$error}}</label><br>
                         @endforeach
                     @endif  
+                    
+                    
+                <input type="submit" class="btn btn-primary" value="cadastrar">
          
         
     </form>
