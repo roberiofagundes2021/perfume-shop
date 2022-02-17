462984530c8aed46db44c1ea8551fa78b6f8635b
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-02-17 13:32:18 -0300

    Update app-index.blade.php

--- resources/views/layouts/app-index.blade.php
+++ resources/views/layouts/app-index.blade.php
@@ -4,10 +4,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=2..0">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
